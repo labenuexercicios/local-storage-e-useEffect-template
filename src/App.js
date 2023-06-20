09ab41889a6a0cd4792f1fc6311c5573bee76072
felipelimars <felipelimars@outlook.com.br>
2023-06-20 12:10:59 -0300

    Exercicios template concluidos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   // Estado para armazenar a lista de compras
@@ -16,6 +16,37 @@ export default function App() {
     }
   };
 
+// coloquei no localStorage
+  const saveLocalStorage = () => {
+//forma resumida em uma linha localStorage.setItem("lista", JSON.stringify(listaCompras))
+const listaString = JSON.stringify(listaCompras)
+localStorage.setItem('lista', listaString)
+}
+
+// peguei do localStorage
+const getItensLocalStorage = () => {
+// transformando em array forma resumida: JSON.parse(localStorage.getItem('lista'))
+const listString2 = localStorage.getItem("lista")
+const listaArray = JSON.parse(listString2)
+if (listaArray) {setListaCompras(listaArray)}
+}
+
+// remover do localStorage
+const removeItemLocalStorage = () => {
+localStorage.removeItem("lista")
+setListaCompras([])
+}
+
+// acontece 1x quando minha pagina é montada
+useEffect(() => {
+  getItensLocalStorage()
+}, [])
+// acontece toda vez que o estado listaCompras é atualizado
+useEffect (() => {
+  if(listaCompras.length){
+    saveLocalStorage()}
+}, [listaCompras])
+
   return (
     <div>
       <h1>Lista de Compras</h1>
@@ -26,6 +57,10 @@ export default function App() {
         placeholder="Digite um item"
       />
       <button onClick={adicionarItem}>Adicionar</button>
+      <button onClick={removeItemLocalStorage}>Remover do localStorage</button>
+{/*   <button onClick={saveLocalStorage}>Salvar no localStorage</button>
+      <button onClick={getItensLocalStorage}>Pegar</button>
+*/} 
 
       <ul>
         {listaCompras.map((compra, index) => (
